21d8f38ec80220f71d22f1490eda3aa22e03e8ef
Crystal_1831 <43097573+Crystak1831@users.noreply.github.com>
2021-09-23 14:26:41 -0400

    add useCallback

--- src/hooks/useCallback/useCallback.js
+++ src/hooks/useCallback/useCallback.js
@@ -1,19 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function UseCallbackDemo() {
   const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
 
-  const getItem = (increment) => {
-    return [number + increment, number + 1 + increment, number + 2 + increment];
-  };
-
-  const changHandler = (e) => {
+  const HandleChange = (e) => {
     setNumber(parseInt(e.target.value));
   };
+
+  //run getItem when number changed:useCallback
+  const getItem = useCallback(
+    (increacement) => {
+      console.log("run list");
+      let arr = [
+        number + increacement,
+        number + 1 + increacement,
+        number + 2 + increacement
+      ];
+      return arr;
+    },
+    [number]
+  );
+
+  const style = {
+    backgroundColor: dark ? "black" : "",
+    color: dark ? "white" : "black"
+  };
+
+  const HandleTheme = () => {
+    setDark(!dark);
+  };
+
   return (
-    <div>
-      <input type="number" onChange={changHandler} value={number} />
-      <button>change theme</button>
+    <div style={style}>
+      <input type="number" value={number} onChange={HandleChange} />
+      <button onClick={HandleTheme}>changeStylr</button>
       <List getItem={getItem} />
     </div>
   );
@@ -22,11 +43,19 @@ export default function UseCallbackDemo() {
 // const List = ({getItem}) =>{return...} 等同于下列写法
 const List = (props) => {
   const { getItem } = props;
-  const [items, setItems] = useState([]);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    setItems(getItem(5));
+    setList(getItem(5));
   }, [getItem]);
 
-  return items.map((item) => <p key={item}>{item}</p>);
+  return (
+    <div>
+      <ul>
+        {list.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
